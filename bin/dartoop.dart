50d344eabeecac457f057b6b482468f811952512
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-12-31 01:02:43 +0500

    Interface and abstract class in OOP dart

--- bin/dartoop.dart
+++ bin/dartoop.dart
@@ -1,6 +1,6 @@
- // 1. ############ Classes in Dart ###############
+// 1. ############ Classes in Dart ###############
 
- // class Microphones {
+// class Microphones {
 //   // Instance variables,member variables
 //   String? name;
 //   String? color;
@@ -55,78 +55,178 @@
 //
 // }
 
- // 2. Inheritance in Dart
+// 2. ########## Inheritance in Dart ##########
 
- // class Person {
- //  String? name , lastName;
- //  int? age;
- //
- //  void showName() {
- //    print(name);
- //  }
- //
- //  void sayHello () => print('HELLO');
- //
- // }
- //
- // class Asad extends Person {
- //  String? profession;
- //
- //  void showPro() => print(profession);
- //
- // }
- //
- // class Devil extends Person {
- //
- //  @override
- //  void sayHello() {
- //    // TODO: implement sayhello
- //    print('Hello from Hell!');
- //  }
- //
- // }
+// class Person {
+//  String? name , lastName;
+//  int? age;
+//
+//  void showName() {
+//    print(name);
+//  }
+//
+//  void sayHello () => print('HELLO');
+//
+// }
+//
+// class Asad extends Person {
+//  String? profession;
+//
+//  void showPro() => print(profession);
+//
+// }
+//
+// class Devil extends Person {
+//
+//  @override
+//  void sayHello() {
+// ignore: todo
+//    // TODO: implement sayhello
+//    print('Hello from Hell!');
+//  }
+//
+// }
 
- class  Location {
-  num? lat,lag;
-  Location(this.lat,this.lag);
-  // Name Constructor
-  Location.create(this.lat,this.lag);
- }
+// class Location {
+//   num? lat, lag;
+//   Location(this.lat, this.lag);
+//   // Name Constructor
+//   Location.create(this.lat, this.lag);
+// }
 
- class ElevatedLocation extends Location {
-  num? total;
-  ElevatedLocation(lat, lag ,this.total) : super(lat, lag);
+// class ElevatedLocation extends Location {
+//   num? total;
+//   ElevatedLocation(lat, lag, this.total) : super(lat, lag);
 
-  @override
-  String toString() {
-    // TODO: implement toString
-    String result = "$lag $lag and $total";
-    return result;
-  }
+//   @override
+//   String toString() {
+// ignore: todo
+//     // TODO: implement toString
+//     String result = "$lag $lag and $total";
+//     return result;
+//   }
+// }
 
- }
+//  void main () {
+
+//   // final asad = Asad();
+
+//   // asad.name = 'Axad';
+//   // asad.showName();
+//   // asad.sayHello();
+
+//   // final devil = Devil();
+
+//   // devil.age = 24;
+//   // print(devil.age);
+//   //
+//   // devil.sayHello();
+
+//   final elevated = ElevatedLocation(20,50,200);
+
+//   // print("Location is = ${elevated.lat} ${elevated.lag} ${elevated.total}");
+
+//   print(elevated.toString());
+
+//  }
+
+// 3. ########## Interface and Abstruct Classes ##########
+
+// abstract class Animal {
+//   void breath();
+
+//   void makeNoise() {
+//     print("Make Noise");
+//   }
+// }
+
+// class Person implements Animal {
+//   String? name, fname;
+
+//   Person(this.name, this.fname);
+
+//   @override
+//   void breath() {
+//     print("Breath thorugh Nostrils");
+//   }
+
+//   @override
+//   void makeNoise() {
+//     print("Human Shout");
+//   }
+
+//   @override
+//   String toString() {
+//     // TODO: implement toString
+//     return "$name  $fname";
+//   }
+// }
+
+// void main() {
+//   final asad = Person('Asad', 'M.Younus');
+
+//   print(asad.toString());
+// }
+
+// abstract class Animal {
+//   void breathe(); // an abstract method
+
+//   void makeNoise() {
+//      print("Making animal noises!");
+//   }
+
+// }
 
 
- void main () {
-
-  // final asad = Asad();
-
-  // asad.name = 'Axad';
-  // asad.showName();
-  // asad.sayHello();
-
-  // final devil = Devil();
-
-  // devil.age = 24;
-  // print(devil.age);
-  //
-  // devil.sayHello();
+// abstract class IsFunny {
+//   void makePeopleLaugh(); //abstract method
+// }
 
 
-  final elevated = ElevatedLocation(20,50,200);
+// class TVShow implements IsFunny {
+//   String name;
 
-  // print("Location is = ${elevated.lat} ${elevated.lag} ${elevated.total}");
+//   @override
+//   void makePeopleLaugh() {
 
-  print(elevated.toString());
 
- }
+
+//      print("TV show is funny and makes people laugh!");
+//   }
+
+// }
+
+// class Comedian extends Person implements IsFunny {
+//   Comedian(String name, String nationality) : super(name, nationality);
+
+//   @override
+//   void makePeopleLaugh() {
+//       print("Comedian making people laugh!");
+//   }
+
+// }
+
+
+
+// class Person implements Animal {
+//    String name, nationality;
+
+
+//    Person(this.name, this.nationality);
+
+//    @override
+//   void breathe() {
+//     print("Person breathing through nostrils!");
+//   }
+
+//   @override
+//   void makeNoise() {
+//     print("Person shouting!");
+//   }
+
+//   @override
+//   String toString() => "$name $nationality";
+
+
+// }
+
